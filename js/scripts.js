81b1f3d0655324caa4db1af6917df399047b161e
njorogedavid13@gmail.com <njorogedavid13@gmail.com>
2017-08-25 17:19:05 +0300

    add the jquery to collect the players name and set the total

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 //Business logic
-function Player(name, total) {
+function Players(name, total) {
   this.name = name
   this.total = total
 }
@@ -10,24 +10,36 @@ var dice = function() {
 
 
 //roll prototype
-Player.prototype.roll = function() {
+Players.prototype.roll = function() {
   var num = dice()
   this.total += num
+  console.log(num);
 }
-
+//function
+var runn = function() {
+  p1.roll()
+  alert('tet')
+}
 
 //user interface
 $(document).ready(function() {
   $('form').submit(function(event) {
-    event.preventDefault
-    var player1 = $('#fplayer').val();
-    var player2 = $('#splayer').val();
+    event.preventDefault();
+    var player1 = $('#fPlayer').val();
+    var player2 = $('#sPlayer').val();
 
-    p1 = new Player(player1, 0)
-    p2 = new Player(player2, 0)
+    p1 = new Players(player1, 0)
+    p2 = new Players(player2, 0)
+    console.log(p1.name);
+    $('#name1').text(p1.name);
+    $('#name2').text(p2.name);
+    $('#total1').text(p1.total);
+    $('#total2').text(p2.total);
+    //roll button
   });
   $('#roll').click(function(event) {
 
+    p2.roll()
 
   });
 });
